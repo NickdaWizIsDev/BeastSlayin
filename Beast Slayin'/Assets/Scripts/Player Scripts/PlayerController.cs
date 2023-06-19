a351abea506df9717fe842fe31e948db628d4bd0
NickdaWizIsDev <nicocastrito2005@gmail.com>
2023-06-19 00:11:50 -0400

    0.2
    Bloom effects set up, basic weapon array set up, idle animation done and implemented, second wall jump added, trail added for testing.

--- Beast Slayin'/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Beast Slayin'/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,7 +15,7 @@ public class PlayerController : MonoBehaviour
     public float runSpeed = 7.5f;
 
     public float jumpImpulse = 7.5f;
-    private readonly float fallGravityScale = 4f;
+    private readonly float fallGravityScale = 7f;
 
     private bool isFacingRight = true;
     public bool IsFacingRight
@@ -36,6 +37,7 @@ public class PlayerController : MonoBehaviour
 
     public AudioSource currentAudioSource;
     public AudioClip swordSwing;
+    private int jumpCount;
 
     void Awake()
     {
@@ -51,7 +53,7 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            rb2d.gravityScale = 1f;
+            rb2d.gravityScale = 4f;
         }
     }
 
@@ -81,13 +83,30 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
+        if (touching.IsGrounded)
+        {
+            jumpCount = 0;
+            Debug.Log("Jumps reset!");
+        }
+
         if (context.started && touching.IsGrounded)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpImpulse);
         }
+        else if (context.started && jumpCount == 0 && touching.IsOnWall)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpImpulse);
+            jumpCount++;
+        }
+        else if (jumpCount < 3 && context.started && touching.IsOnWall)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpImpulse);
+            jumpCount++;
+        }
         else if (context.canceled)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
+            rb2d.gravityScale = fallGravityScale;
         }
     }
 }
